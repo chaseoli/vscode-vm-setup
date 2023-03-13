1c85cb6d9af9709432439cd3078ef950a5a03e37
Chase <chase@bravo6.io>
2023-03-13 13:50:03 -0700

    update

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 NON_ROOT_USER=$1
-GIT_USERNAME=$2
-GIT_EMAIL=$3
+# GIT_USERNAME=$2
+# GIT_EMAIL=$3
 
 CURRENT_USER=$(whoami)
 
     echo "Your are Logged-in as $CURRENT_USER."
 
-if  [[ -z "$1"  ||  -z "$2" || "$CURRENT_USER" != "root" ]]; then
+if  [[ -z "$1"  || "$CURRENT_USER" != "root" ]]; then
     printf "\n You must be logged-in as root and pass the name and password of the user you would like to create."
     printf "\n For example: bash install.sh <USERNAME_HERE> <PASSWORD_HERE>\n\n"
     exit 1
@@ -23,18 +23,15 @@ else
     adduser --gecos "" $NON_ROOT_USER
     usermod -aG sudo $NON_ROOT_USER
 
-    # login as new user
-    su $NON_ROOT_USER
-
     # install updates and upgrade outdated packages
-    sudo apt update && sudo apt upgrade
-
+    apt update && apt upgrade
+    
     # install nodejs
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
     source ~/.bashrc 
 
     # install git
-    sudo apt install git
+    apt install git
 
     # # setup git
     # git config --global user.name "Your Name"
@@ -43,26 +40,27 @@ else
     # install vscode server
     wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
 
-  
-
     # install global node packages
     npm i -g typescript firebase-tools @loopback/cli @angular/cli mocha 
 
     # install the OpenJDK JRE:
-    sudo apt install default-jre
+    apt install default-jre
     java -version
 
     # install python 
-    sudo apt-get install python3.6
+    apt-get install python3.6
     python3 --version
     command -v pip3
 
     # install go
     curl -OL https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
     # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+    tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
     export PATH=$PATH:/usr/local/go/bin
     go version
+
+    # login as new user
+    su $NON_ROOT_USER
 
     # # start code server (to be accessed remotely)
     # code-server --accept-server-license-terms
