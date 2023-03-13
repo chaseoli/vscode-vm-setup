04a339cf2b9387e80947d4f13aff8f27cccc8732
Chase <chase@bravo6.io>
2023-03-13 14:36:12 -0700

    update install script

--- install.sh
+++ install.sh
@@ -1,68 +1,50 @@
 #!/usr/bin/env bash
 
-NON_ROOT_USER=$1
-# GIT_USERNAME=$2
-# GIT_EMAIL=$3
+# install sudo
+apt-get install sudo
 
-CURRENT_USER=$(whoami)
+# install updates and upgrade outdated packages
+apt update -y && apt upgrade -y
 
-    echo "Your are Logged-in as $CURRENT_USER."
+# get curl
+apt install curl -y
 
-if  [[ -z "$1"  || "$CURRENT_USER" != "root" ]]; then
-    printf "\n You must be logged-in as root and pass the name and password of the user you would like to create."
-    printf "\n For example: bash install.sh <USERNAME_HERE> <PASSWORD_HERE>\n\n"
-    exit 1
-else
+# install nodejs
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source ~/.bashrc 
 
-    apt-get install sudo
+# get nvm 
+nvm -v
+nvm install --lts
+nvm use --lts 
 
-    echo "Creating non-root user: $NON_ROOT_USER"
+# get node version
+node -v
+npm -v
 
-    # add username and password
-    # adduser --disabled-password --gecos "" $NON_ROOT_USER
-    adduser --gecos "" $NON_ROOT_USER
-    usermod -aG sudo $NON_ROOT_USER
+# install global node packages
+npm i -g typescript firebase-tools @loopback/cli @angular/cli mocha 
 
-    # install updates and upgrade outdated packages
-    apt update && apt upgrade
-    
-    # install nodejs
-    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-    source ~/.bashrc 
+# install git
+apt install git -y
 
-    # install git
-    apt install git
+# install vscode server
+wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
 
-    # # setup git
-    # git config --global user.name "Your Name"
-    # git config --global user.email "youremail@domain.com"
+# install the OpenJDK JRE:
+apt install default-jre -y
+java -version
 
-    # install vscode server
-    wget -O- https://aka.ms/install-vscode-server/setup.sh | sh
+# install python 
+apt-get install python3.6 -y
+python3 --version
+apt install python3-pip -y
+pip3 -V
 
-    # install global node packages
-    npm i -g typescript firebase-tools @loopback/cli @angular/cli mocha 
+# install go
+curl -OL https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
+# rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
+export PATH=$PATH:/usr/local/go/bin
+go version
 
-    # install the OpenJDK JRE:
-    apt install default-jre
-    java -version
-
-    # install python 
-    apt-get install python3.6
-    python3 --version
-    command -v pip3
-
-    # install go
-    curl -OL https://golang.org/dl/go1.20.2.linux-amd64.tar.gz
-    # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-    tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz
-    export PATH=$PATH:/usr/local/go/bin
-    go version
-
-    # login as new user
-    su $NON_ROOT_USER
-
-    # # start code server (to be accessed remotely)
-    # code-server --accept-server-license-terms
-
-fi
